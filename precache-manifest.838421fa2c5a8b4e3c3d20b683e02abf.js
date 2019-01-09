2437af69a2abe1276b7bcfb800cf863fffeebe18
John McNeill <jh_mcneill@yahoo.ca>
2019-01-09 11:52:51 -0500

    Updates

---
+++ precache-manifest.838421fa2c5a8b4e3c3d20b683e02abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/javascript-calculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9bc7773250f7c400c052",
+    "url": "/javascript-calculator/static/js/runtime~main.9bc77732.js"
+  },
+  {
+    "revision": "131e4c4646cd8b0008d3",
+    "url": "/javascript-calculator/static/js/main.131e4c46.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/javascript-calculator/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "131e4c4646cd8b0008d3",
+    "url": "/javascript-calculator/static/css/main.93746c75.chunk.css"
+  },
+  {
+    "revision": "a55c961060bc5ac9fa0d0731fb11baa2",
+    "url": "/javascript-calculator/index.html"
+  }
+];
